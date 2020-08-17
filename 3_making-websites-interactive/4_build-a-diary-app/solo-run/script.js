81d341c92a9e708eaf65fac57d46fb59d4f0cd19
Michael080 <mrollin808@gmail.com>
2020-08-16 22:42:30 -0600

    Create buttons for individual entries

--- 3_making-websites-interactive/4_build-a-diary-app/solo-run/script.js
+++ 3_making-websites-interactive/4_build-a-diary-app/solo-run/script.js
@@ -1,8 +1,23 @@
 const form = document.querySelector('#text-input');
+const textField = document.querySelector('#box');
 const button = document.querySelector('#submit');
+const entrySection = document.querySelector('.entry');
+const nav = document.querySelector('.entries-nav');
+
+let counter = 0;
 
 function userSubmit(event){
-    event.preventDefault();
+    event.preventDefault();  //Prevent refresh
+    let userInput = textField.value;  //Save user input
+    //Create output text and append to DOM
+    let outputText = document.createElement('div');
+    outputText.textContent = userInput;
+    outputText.style.display = 'none';
+    //Create buttons for each entry
+    let toggleText = document.createElement('button');
+    counter++;
+    toggleText.innerText = counter;
+    nav.appendChild(toggleText);
 }
 
 form.addEventListener('submit', userSubmit);
