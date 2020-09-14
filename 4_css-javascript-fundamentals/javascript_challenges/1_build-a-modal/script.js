b08a87a08ac447aac65803fe73eceacc9cb49827
Michael080 <mrollin808@gmail.com>
2020-09-13 18:29:42 -0600

    Dim background on click of contact button

--- 4_css-javascript-fundamentals/javascript_challenges/1_build-a-modal/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/1_build-a-modal/script.js
@@ -1,6 +1,10 @@
 const contactButton = document.querySelector('#contact-btn');
 const modal = document.querySelector('.card');
+const contactHeading = document.querySelector('.contact');
 
 contactButton.addEventListener('click', function(){
 modal.classList.remove('hidden');
+document.body.style.backgroundColor = '#a9a9a9';
+contactButton.classList.toggle('opaque');
+contactHeading.classList.toggle('opaque');
 });
