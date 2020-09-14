f88bd8a2097af68f73370919829c9a6ab99b1c37
Michael080 <mrollin808@gmail.com>
2020-09-13 18:42:07 -0600

    Hide modal and undim background on click of close button & declare and use function in event listeners

--- 4_css-javascript-fundamentals/javascript_challenges/1_build-a-modal/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/1_build-a-modal/script.js
@@ -1,10 +1,15 @@
 const contactButton = document.querySelector('#contact-btn');
 const modal = document.querySelector('.card');
 const contactHeading = document.querySelector('.contact');
+const closeButton = document.querySelector('#close-btn');
 
-contactButton.addEventListener('click', function(){
-modal.classList.remove('hidden');
-document.body.style.backgroundColor = '#a9a9a9';
-contactButton.classList.toggle('opaque');
-contactHeading.classList.toggle('opaque');
-});
+contactButton.addEventListener('click', toggleDimBackground);
+
+closeButton.addEventListener('click', toggleDimBackground);
+
+function toggleDimBackground(){
+    modal.classList.toggle('hidden');
+    document.body.classList.toggle('gray-background');
+    contactButton.classList.toggle('opaque');
+    contactHeading.classList.toggle('opaque');
+}
