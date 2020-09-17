dd1d622bd9b6f92779333202d1fdbe8cdd68c711
Michael080 <mrollin808@gmail.com>
2020-09-16 22:24:06 -0600

    Store user keystrokes and print to console

--- 4_css-javascript-fundamentals/javascript_challenges/7_build-a-search-bar/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/7_build-a-search-bar/script.js
@@ -4,3 +4,11 @@
     2. Make sure the captured value is lowercase
     3. Log to the console to demonstrate it works
 */
+
+const input = document.querySelector('#searchInput');
+
+input.addEventListener('keyup', function(event){
+    let searchQuery = event.key.toLowerCase();
+
+    console.log(searchQuery);
+})
