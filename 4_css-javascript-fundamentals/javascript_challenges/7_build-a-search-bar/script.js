79bfaa673a18ad89c4509fa9503761d61779a01d
Michael080 <mrollin808@gmail.com>
2020-09-17 21:54:13 -0600

    Store individual name in currentName and log name

--- 4_css-javascript-fundamentals/javascript_challenges/7_build-a-search-bar/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/7_build-a-search-bar/script.js
@@ -12,13 +12,18 @@ let allNamesDOMCollection = [];
 nameNodes.forEach((name) => {
     allNamesDOMCollection.push(name.textContent);
 });
-console.log(
-    'allNamesDOMCollection', allNamesDOMCollection, '\n',
-    'first index: ', allNamesDOMCollection[0]
-);
+// console.log(
+//     'allNamesDOMCollection', allNamesDOMCollection, '\n',
+//     'first index: ', allNamesDOMCollection[0]
+// );
+
+for(let i = 0; i < allNamesDOMCollection.length; i++){
+    const currentName = allNamesDOMCollection[i];
+    console.log('currentName: ', currentName);
+}
 
 input.addEventListener('keyup', function(event){
     let searchQuery = event.target.value.toLowerCase();
-    console.log('event.target.value:', searchQuery);
+    // console.log('event.target.value:', searchQuery);
 });
 
