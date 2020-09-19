d7e78d671368f26093b98fae7c801ba6ccbb414e
Michael080 <mrollin808@gmail.com>
2020-09-19 17:10:20 -0600

    Add comments

--- 4_css-javascript-fundamentals/javascript_challenges/7_build-a-search-bar/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/7_build-a-search-bar/script.js
@@ -2,8 +2,10 @@ const input = document.querySelector('#searchInput');
 const nameNodes = document.querySelectorAll('.name');
 let allNamesDOMCollection = [];
 
+// Reset search field on page load
 input.value = '';
 
+//Store name values from DOM in an array
 nameNodes.forEach((name) => {
     allNamesDOMCollection.push(name.textContent);
 });
@@ -12,12 +14,13 @@ input.addEventListener('keyup', event => {
     search(event);
 });
 
+//Find matching string values
 function search (event) {
     let searchQuery = event.target.value.toLowerCase();
 
     for(let i = 0; i < allNamesDOMCollection.length; i++){
         const currentName = allNamesDOMCollection[i].toLowerCase();
-
+        //Display matches
         if(currentName.includes(searchQuery)){
             nameNodes[i].style.display = 'block';
         } else {
