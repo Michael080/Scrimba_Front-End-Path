2d09802de90714956a152115d14aeea72c312902
Michael080 <mrollin808@gmail.com>
2020-09-20 00:56:40 -0600

    Create left and right buttons for clicking through the carousel

--- 4_css-javascript-fundamentals/javascript_challenges/13_build-a-carousel/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/13_build-a-carousel/script.js
@@ -4,17 +4,20 @@ let images = [];
 //Add images to array via <img>s
 let jokerImage = document.createElement('img');
 jokerImage.setAttribute('src', 'images/1.jpg');
+jokerImage.setAttribute('class', 'image');
 images.push(jokerImage);
 
 let babyFace = document.createElement('img');
 babyFace.setAttribute('src', 'images/2.jpg');
+babyFace.setAttribute('class', 'image');
 images.push(babyFace);
 
 let blockedEyes = document.createElement('img');
 blockedEyes.setAttribute('src', 'images/3.jpg');
+blockedEyes.setAttribute('class', 'image');
 images.push(blockedEyes);
 
-
+displayImage(images[0]);
 
 function displayImage(image) {
     carousel.appendChild(image);
