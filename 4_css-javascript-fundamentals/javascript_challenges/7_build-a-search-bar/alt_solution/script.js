119cafd546b51bceac76a49a69855b9fce27c1f4
Michael080 <mrollin808@gmail.com>
2020-09-30 19:35:27 -0600

    Declare necessary variables and selectors

---
+++ 4_css-javascript-fundamentals/javascript_challenges/7_build-a-search-bar/alt_solution/script.js
@@ -0,0 +1,6 @@
+const previous = document.querySelector('#carousel-button-previous');
+const next = document.querySelector('#carousel-button-next');
+const imageCollection = document.querySelectorAll('img');
+
+let counter = 0;
+let currentImage = imageCollection[counter];
