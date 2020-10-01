8c7d3b9338026bf824d7448789c4aa721e2e85da
Michael080 <mrollin808@gmail.com>
2020-09-30 22:05:16 -0600

    Change image on click of 'previous' and 'next' buttons'

--- 4_css-javascript-fundamentals/javascript_challenges/7_build-a-search-bar/alt_solution/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/7_build-a-search-bar/alt_solution/script.js
@@ -1,6 +1,51 @@
+//DOM Selectors
 const previous = document.querySelector('#carousel-button-previous');
 const next = document.querySelector('#carousel-button-next');
 const imageCollection = document.querySelectorAll('img');
 
-let counter = 0;
-let currentImage = imageCollection[counter];
+let counter = 0;  //for iteration
+//Reference values
+let start = 0;
+let end = imageCollection.length - 1;
+
+previous.addEventListener('click', changeSlide);
+next.addEventListener('click', changeSlide);
+
+function changeSlide(event){
+    let buttonClicked = event.target.classList[0];
+    let currentImage = imageCollection[counter];
+    //Hide image
+    toggleImage(currentImage);
+
+    if (buttonClicked == 'previous'){
+        previousImage();
+    } else {
+        nextImage();
+    }
+}
+
+function previousImage(){
+    if (counter == start){  //check if at start of collection
+        counter = 2;
+    } else {
+        counter--;
+    }
+    let previousImage = imageCollection[counter];
+    toggleImage(previousImage);
+}
+
+function nextImage(){
+    if (counter == end){
+        counter = 0;
+    } else {
+        counter++;
+    }
+    let nextImage = imageCollection[counter];
+    toggleImage(nextImage);
+}
+
+function toggleImage(image){
+    image.parentNode.classList.toggle('carousel-item-visible');
+}
+
+
