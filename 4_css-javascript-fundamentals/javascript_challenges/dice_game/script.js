7c15627541ae8367b2dbedc7434713cbdd98737f
Michael Rollin <mrollin808@gmail.com>
2020-10-19 23:45:07 -0600

    Create function rollDice() that returns a random number between 1 and 6

--- 4_css-javascript-fundamentals/javascript_challenges/dice_game/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/dice_game/script.js
@@ -1,13 +1,21 @@
-let playerTurn = document.querySelector('.player-turn');
-let roll = document.querySelector('.roll'); //roll button
+const message = document.querySelector('.message');
+const roll = document.querySelector('.roll');
+const reset = document.querySelector('reset');
 //player one stats
-let oneScore = document.querySelector('.player-one-score');
-let diceOne = document.querySelector('.dice-one');
+const oneScore = document.querySelector('.player-one-score');
+const diceOne = document.querySelector('.dice-one');
 //player two stats
-let twoScore = document.querySelector('.player-two-score');
-let diceTwo = document.querySelector('.dice-two');
+const twoScore = document.querySelector('.player-two-score');
+const diceTwo = document.querySelector('.dice-two');
 
 // game stats
 player1 = 0;
 player1Turn = true;
 player2 = 0;
+
+// rollDice() - returns random number range 1 - 6
+let rollDice = () => {
+    return Math.floor((Math.random() * 6) + 1);
+}
+
+console.log('rollDice => ', rollDice());
