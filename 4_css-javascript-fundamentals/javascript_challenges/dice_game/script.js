ae857f99d33a7e874bb365475e34ef6e9abbb629
Michael Rollin <mrollin808@gmail.com>
2020-10-19 21:06:18 -0600

    Style page

---
+++ 4_css-javascript-fundamentals/javascript_challenges/dice_game/script.js
@@ -0,0 +1,14 @@
+// player roll
+
+
+// player score
+let dice = {
+    scoreboard: {
+        player1: 0,
+        player1Turn: true,
+        player2: 0
+    }
+}
+
+console.log(`player1: score - ${dice.scoreboard.player1}`);
+console.log(`player2: score - ${dice.scoreboard.player2}`);
