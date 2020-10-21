86e98ef2f37d389b53e6ea721746dbf8be730a3a
Michael Rollin <mrollin808@gmail.com>
2020-10-20 23:29:08 -0600

    Toggle active-class on both dice on click

--- 4_css-javascript-fundamentals/javascript_challenges/dice_game/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/dice_game/script.js
@@ -7,16 +7,19 @@ const diceOne = document.querySelector('.dice-one');
 //player two stats
 const twoScore = document.querySelector('.player-two-score');
 const diceTwo = document.querySelector('.dice-two');
-
 // game stats
 let player1 = 0;
-let player1Turn = true;
+let player1Turn = false;
 let player2 = 0;
-
 
 let randNumb = () => {
     return Math.floor((Math.random() * 6) + 1);
 }
+
+let toggleElems = (currNode, otherNode) => {
+    currNode.classList.add('active');
+    otherNode.classList.remove('active');
+};
 
 let updateScore = (player, currentRoll, playerBoard) => {
     player += currentRoll;
@@ -24,32 +27,26 @@ let updateScore = (player, currentRoll, playerBoard) => {
     return player;
 }
 
-let scoring = (player, dice) => {
+let scoring = (player, currDice, otherDice) => {
     let playerRoll = randNumb();
-    dice.textContent = playerRoll;
+    currDice.textContent = playerRoll;
+    toggleElems(currDice, otherDice);
     return playerRoll;
 }
 
 function playDice() {
+    player1Turn = !player1Turn;
     if (player1Turn) {
         message.textContent = 'Player 1 Turn';
-
-        let playerRoll = scoring(player1, diceOne);
+        let playerRoll = scoring(player1, diceOne, diceTwo);
         player1 = updateScore(player1, playerRoll, oneScore);
-        player1Turn = false;
         return player1;
     } else {
         message.textContent = 'Player 2 Turn';
-        let playerRoll = scoring(player2, diceTwo);
+        let playerRoll = scoring(player2, diceTwo, diceOne);
         player2 = updateScore(player2, playerRoll, twoScore);
-        player1Turn = true;
         return player2;
     }
 }
 
 roll.addEventListener('click', playDice);
-
-
-// rollDice() - returns random number range 1 - 6
-
-// console.log('rollDice => ', rollDice());
