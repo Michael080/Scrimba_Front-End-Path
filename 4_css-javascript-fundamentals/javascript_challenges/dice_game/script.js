de7f9d8d1f9abebd8442309ba6ca194e82c96c90
Michael Rollin <mrollin808@gmail.com>
2020-10-20 21:25:04 -0600

    Create logic for dice rolls, scoring, and update scoreboard/dice roll info

--- 4_css-javascript-fundamentals/javascript_challenges/dice_game/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/dice_game/script.js
@@ -9,13 +9,47 @@ const twoScore = document.querySelector('.player-two-score');
 const diceTwo = document.querySelector('.dice-two');
 
 // game stats
-player1 = 0;
-player1Turn = true;
-player2 = 0;
+let player1 = 0;
+let player1Turn = true;
+let player2 = 0;
 
-// rollDice() - returns random number range 1 - 6
-let rollDice = () => {
+
+let randNumb = () => {
     return Math.floor((Math.random() * 6) + 1);
 }
 
-console.log('rollDice => ', rollDice());
+let updateScore = (player, currentRoll, playerBoard) => {
+    player += currentRoll;
+    playerBoard.textContent = player;
+    return player;
+}
+
+let scoring = (player, dice) => {
+    let playerRoll = randNumb();
+    dice.textContent = playerRoll;
+    return playerRoll;
+}
+
+function playDice() {
+    if (player1Turn) {
+        message.textContent = 'Player 1 Turn';
+
+        let playerRoll = scoring(player1, diceOne);
+        player1 = updateScore(player1, playerRoll, oneScore);
+        player1Turn = false;
+        return player1;
+    } else {
+        message.textContent = 'Player 2 Turn';
+        let playerRoll = scoring(player2, diceTwo);
+        player2 = updateScore(player2, playerRoll, twoScore);
+        player1Turn = true;
+        return player2;
+    }
+}
+
+roll.addEventListener('click', playDice);
+
+
+// rollDice() - returns random number range 1 - 6
+
+// console.log('rollDice => ', rollDice());
