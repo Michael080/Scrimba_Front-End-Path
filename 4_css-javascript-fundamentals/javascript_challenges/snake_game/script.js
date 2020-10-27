86141ef75ff824e8eaae9ff1e329149967a877b7
Michael Rollin <mrollin808@gmail.com>
2020-10-26 21:08:09 -0600

    Create game board by converting node collection to array and slicing in 10 index increments

--- 4_css-javascript-fundamentals/javascript_challenges/snake_game/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/snake_game/script.js
@@ -1,6 +1,24 @@
+const areaGrid = document.querySelectorAll('.grid');
+
 let snek = {
     body: 3,
     position: 0, //make
     speed: 1,
     direction: 'down',
 }
+
+// Slice grid into 10-index sections and store
+let makeBoard = (array, tempBoard, section = 10) => {
+    for (let i = 0; i < array.length - 1; i += section) {
+        // var a = new Array;
+        // a.push(array.slice(i, i + section));
+        let a = array.slice(i, i + section);
+        tempBoard.push(a);
+        console.log(a);
+    }
+    return tempBoard;
+}
+
+let tempGrid = Array.from(areaGrid); //convert node collection
+let arrayGrid = new Array;
+let snekLand = makeBoard(tempGrid, arrayGrid);
