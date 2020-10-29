1a01df2aa5e9ef0dc1c8209a29f271cd3c1a451c
Michael Rollin <mrollin808@gmail.com>
2020-10-28 22:53:02 -0600

    Store snek location on snek-object

--- 4_css-javascript-fundamentals/javascript_challenges/snake_game/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/snake_game/script.js
@@ -1,6 +1,14 @@
-const makeBoard = require('model/makeBoard.js');
+// const makeBoard = require('./gameboard/makeBoard.js');
 
 const areaGrid = document.querySelectorAll('.grid');
+
+function makeBoard(array, tempBoard, section = 10) {
+    for (let i = 0; i < array.length - 1; i += section) {
+        let a = array.slice(i, i + section);
+        tempBoard.push(a);
+    }
+    return tempBoard;
+}
 
 let tempGrid = Array.from(areaGrid); //convert node collection
 let arrayGrid = new Array;
@@ -12,12 +20,19 @@ function Snek(body, position, speed, direction) {
     this.position = position;
     this.speed = 0;
     this.direction = direction;
+    this.setSnekPlace = function (snekker) {
+        this.position = snekker;
+    }
 }
 
 let snek = new Snek (3,0,1,'down'); //new Snek
 
-// function add(num1, num2) {
-//     return num1 + num2;
-// }
-//
-// module.exports = add;
+Snek.prototype.findSnek = function (board) {
+        board.forEach(gridRow => {
+        gridRow.forEach(grid => {
+            grid.classList.contains('snek') ? (this.setSnekPlace(grid)) : 'no snek'
+        });
+    }
+)}
+
+snek.findSnek(snekLand);
