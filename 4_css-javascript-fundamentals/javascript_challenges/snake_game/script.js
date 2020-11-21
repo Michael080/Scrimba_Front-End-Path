89abdfb85f96da692e9db81912fcc43b34c8fd7e
Michael Rollin <mrollin808@gmail.com>
2020-11-21 16:31:58 -0700

    Reorganize code, add section labels, & improve/add comments

--- 4_css-javascript-fundamentals/javascript_challenges/snake_game/script.js
+++ 4_css-javascript-fundamentals/javascript_challenges/snake_game/script.js
@@ -23,6 +23,7 @@ function Snek(size, position, speed, direction, restrict, boundCheck, ded) {
     this.restrict = {
         movement: false
     };
+    // TODO --- boundCheck unused - delete or donate name to appropriate function/data-structure/thingamajig
     this.boundCheck = boundCheck;
     this.ded = false;
 
@@ -44,7 +45,7 @@ function Snek(size, position, speed, direction, restrict, boundCheck, ded) {
     //specs type of out-of-bounds condition and a bool value
     this.checker = function (grid, row, board) {
         const xBound = this.position.row.dom.length - 1;
-        const yBound = board.length - 2;
+        const yBound = board.length - 1;
         // TODO --- Use Map instead of Object
         // Check for out-of-bound conditions on x/y axis
         const lessX = {lessX: grid < 0};
@@ -67,7 +68,6 @@ function Snek(size, position, speed, direction, restrict, boundCheck, ded) {
     } //<----- end of checker()
 
     // Updates restrict property with bool and restricted direction if applicable
-    // snek.outOfBound(newPos, snek.position.row.y, snekLand);
     //newPos --- array index
     //snek.position.row.y --- index of array in snekLand
     this.outOfBound = function (grid, row, board) {
@@ -78,7 +78,6 @@ function Snek(size, position, speed, direction, restrict, boundCheck, ded) {
                     movement: true,
                     dir: 'left'
                 },
-                // TODO --- Fix Bug - snek out of bounds NO game abort - Not returning on out of bound conditions
                 greatX: () => snek.restrict = {
                     movement: true,
                     dir: 'right'
@@ -87,7 +86,6 @@ function Snek(size, position, speed, direction, restrict, boundCheck, ded) {
                     movement: true,
                     dir: 'up'
                 },
-                // TODO --- Fix Bug - snek out of bounds NO game abort - Not returning on out of bound conditions
                 greatY: () => snek.restrict = {
                     movement: true,
                     dir: 'down'
@@ -104,7 +102,8 @@ function Snek(size, position, speed, direction, restrict, boundCheck, ded) {
         } catch(err) {
             restrictWhich['defaultResult'];
         }
-    }
+
+    }//<========= END OF SNEK PROTOTYPE
 
     this.stopSnek = () => {
         this.restrict.movement = true;
@@ -120,6 +119,7 @@ function Snek(size, position, speed, direction, restrict, boundCheck, ded) {
                 });
             }
         )}
+    //    TODO --- Bug - snek not responsive to input at top and bottom rows newPos not running?
     // Returns DOM element corresponding to left/right/up/down
     this.newPos = function (dir) {
         const currentCoords = {
@@ -181,13 +181,75 @@ function Snek(size, position, speed, direction, restrict, boundCheck, ded) {
                 return newCoords;
             }
         }
+        // console.log('newCoords', posSwitch[dir]);
         return posSwitch[dir];
     } // <----- end of newPos()
  }
 
+
+ // ----------------------------     Create Snek     ----------------------------
 // Create snek, set position and moves
 const snek = new Snek (3,3000,500,'left'); //new Snek
 snek.findSnek(snekLand); //set snek.position
+snek.firstMove = true;
+
+
+// ----------------------------     Display Snek     ----------------------------
+// Functions for toggling CLASSES and IDs
+const toggleClass = (elem, selector = 'tail') => elem.classList.toggle(selector);
+const toggleID = (elem, selector = '') => elem.id = selector;
+
+// Adds class-'tail' and sets timer to remove after spec'd time
+const setTailClass = (elem, selector) => {
+    toggleClass(elem, selector); //add 'tail'
+}
+
+// add new id #snek and turn 'old snek' into a class .tail
+const toggleSnek = ( currPos, nextPos ) => {
+    toggleID(currPos); //remove id-'snek'
+    setTailClass(currPos, 'tail'); //add class-'tail'
+    toggleID(nextPos, 'snek'); //New snek is created
+    setTimeout(() => toggleClass(nextPos, 'tail'), snek.speed * (snek.size));
+
+    if (snek.firstMove === true) {
+        initialDraw();
+        setTimeout(() => toggleClass(currPos, 'tail'), snek.speed * (snek.size - 1));
+        return snek.firstMove = false;
+    }
+}
+
+// Locate the first tail parts of snek
+const setTailPos = (snek) => {
+    console.log('drawing snek...');
+    const tail = length => {
+        let referencePoint = snek.position.row.y;
+        let snekTail = [];
+        for (let i = 0; i < length - 1; i ++) {
+            let tailPos = referencePoint + 1;
+            let tail = snek.position.row.dom[tailPos];
+            snekTail.push(tail);
+            referencePoint = tailPos;
+        }
+        snek.initialTailPos = snekTail;
+    }
+    return tail(snek.size);
+}
+
+// Set position & display the rest of Snek body for the first move cycle of game
+function initialDraw() {
+    setTailPos(snek);
+    const tail1 = snek.initialTailPos[0];
+    const tail2 = snek.initialTailPos[1];
+    //draw tail and set timers to 'fade' display:
+    toggleClass(tail1, 'tail'); //draw 'tail'
+    toggleClass(tail2, 'tail'); //draw 'tail'
+    setTimeout(() => toggleClass(tail1, 'tail'), snek.speed * (snek.size - 2));
+    toggleClass(tail2, 'tail');
+    delete snek.initialTailPos; //remove property from Snek object
+}
+
+
+// ----------------------------     Move Snek     ----------------------------
 // * TODO---Implement in prototype & use in outOfBound() //
 // Calc next grid position based on move direction and current position
 snek.moves = {
@@ -205,54 +267,36 @@ snek.moves = {
     }
 }
 
-// Functions for toggling CLASSES and IDs
-const toggleClass = (elem, selector = 'tail') => elem.classList.toggle(selector);
-
-// Adds class-'tail' and sets timer to remove after spec'd time
-const setTail = (elem, selector) => {
-   toggleClass(elem, selector); //add 'tail'
-}
-
-const toggleID = (elem, selector = '') => elem.id = selector;
-snek.firstMove = true;
-
-// add new 'snek' and turn 'old snek' into a 'tail'
-const toggleSnek = ( currPos, nextPos ) => {
-    toggleID(currPos); //remove id-'snek'
-    setTail(currPos, 'tail'); //add class-'tail'
-    toggleID(nextPos, 'snek'); //New snek is created
-    setTimeout(() => toggleClass(nextPos, 'tail'), snek.speed * (snek.size));
-
-    if (snek.firstMove === true) {
-        initialDraw();
-        setTimeout(() => toggleClass(currPos, 'tail'), snek.speed * (snek.size - 1));
-       return snek.firstMove = false;
-    }
-}
-
-const setTailPos = (snek) => {
-    console.log('drawing snek...');
-    const tail = length => {
-        let referencePoint = snek.position.row.y;
-        let snekTail = [];
-        for (let i = 0; i < length - 1; i ++) {
-            let tailPos = referencePoint + 1;
-            let tail = snek.position.row.dom[tailPos];
-            snekTail.push(tail);
-            referencePoint = tailPos;
-        }
-        snek.initialTailPos = snekTail;
-    }
-    return tail(snek.size);
-}
-// TODO --- move execution somewhere else in flow
-// setTailPos(snek); //draw snek
-
 // Validate move
 const isValidMove = function (dir) {
     const newPos = snek.moves[dir]();
     // Check next snek-position: if out-of-bounds & update snek.restrict v/ validation
     snek.outOfBound(newPos, snek.position.row.y, snekLand);
+}
+
+function move() {
+    const x = snek.position.grid.x;
+    const y = snek.position.row.y;
+    isValidMove(snek.direction); //validate move
+    if (snek.restrict.movement === false) {
+        //set snek.position to grid location containing id #snek
+        let pos = snek.position.grid.dom;
+        let nextPos = snek.newPos(snek.direction);
+        //set nextPos w/ id #snek, remove #snek from pos & swap w/ class .tail
+        // TODO --- Move Utilities -- move toggle utils - move toggleSnek()
+        toggleSnek(pos, nextPos);
+        snek.findSnek(snekLand); //reset snek.position
+    }else {
+    //    GAME OVER
+        snek.stopSnek();
+        snek.ded = true;
+        console.log('Snek Ded :(');
+        return clearInterval(timer);
+    }
+}
+
+function clearTimer(timer) {
+    return clearInterval(timer);
 }
 
 function goTimer(ref, timerType, comm, speed = snek.speed) {
@@ -271,68 +315,13 @@ function goTimer(ref, timerType, comm, speed = snek.speed) {
 // mine = timer(mine, 'start');
 // timer(mine, 'stop');
 // clearInterval(mine);
-
 // snekGo = goTimer(snekGo, setTimeout, 'start');
-// TODO --- initialDraw() should be a method on Snek proto
-//      - toggle utilities must ALSO be methods on Snek proto
-function initialDraw() {
-    //set position & draw the rest of Snek body:
-    setTailPos(snek);
-    const tail1 = snek.initialTailPos[0];
-    const tail2 = snek.initialTailPos[1];
-    //draw tail and set timers to 'fade' display:
-    toggleClass(tail1, 'tail'); //draw 'tail'
-    toggleClass(tail2, 'tail'); //draw 'tail'
-    setTimeout(() => toggleClass(tail1, 'tail'), snek.speed * (snek.size - 2));
-    toggleClass(tail2, 'tail');
-    delete snek.initialTailPos; //remove property from Snek object
-}
-
-let timer = setInterval(move, snek.speed); //set movement based on snek.speed
-
-function move() {
-    const x = snek.position.grid.x;
-    const y = snek.position.row.y;
-    isValidMove(snek.direction); //validate move
-    if (snek.restrict.movement === false) {
-        //set snek.position to grid location containing id #snek
-        let pos = snek.position.grid.dom;
-        let nextPos = snek.newPos(snek.direction);
-        //set nextPos w/ id #snek, remove #snek from pos & swap w/ class .tail
-        toggleSnek(pos, nextPos);
-        snek.findSnek(snekLand); //reset snek.position
-    }else {
-    //    GAME OVER
-        snek.stopSnek();
-        snek.ded = true;
-        console.log('Snek Ded :(');
-        clearInterval(timer);
-        return clearInterval(timer);
-    }
-}
-
-const moveOverride = (input) => {
-    snek.direction = input;
-    const x = snek.position.grid.x;
-    const y = snek.position.row.y;
-    isValidMove(input); //validate move
-    if (snek.restrict.movement === false) {
-        let pos = snek.position.grid.dom;
-        let nextPos = snek.newPos(snek.direction);
-        toggleSnek(pos, nextPos);
-        snek.findSnek(snekLand);
-        // setInterval(move(snek.direction), 500);
-    }else {
-        //    GAME OVER
-        console.log('Snek Ded :(');
-        return clearInterval(moveTimer);
-    }
-}
-
+// Set snek.direction based on user input
 const snekControl = function(event) {
-    snek.direction = event.slice(5, input.length - 1);
+    snek.direction = event.slice(5, input.length - 1); //remove 'Arrow' from user input string
 }
 
+// Check formatted input for valid values => 'right, left, up, down'
 function validateInput(input) {
     const valid = ['right', 'left', 'up', 'down'];
     return valid.includes(input);
@@ -348,12 +337,11 @@ window.addEventListener('keydown', function(event) {
     } else {
         console.log('invalid input: ', event.key);
     }
-
 });
 
-function clearTimer(timer) {
-    return clearInterval(timer);
-}
+
+// ----------------------------     Start Game     ----------------------------
+let timer = setInterval(move, snek.speed); //set movement based on snek.speed
 
 //--------------------------------------------------
 // TESTS:
