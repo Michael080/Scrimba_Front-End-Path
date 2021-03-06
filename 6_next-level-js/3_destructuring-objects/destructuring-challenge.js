c4ad38e2e259b2f453f7d20d8bb2ba83d27eded8
Michael Rollin <mrollin808@gmail.com>
2021-03-05 17:32:43 -0700

    Destructuring challenge.

---
+++ 6_next-level-js/3_destructuring-objects/destructuring-challenge.js
@@ -0,0 +1,22 @@
+/*
+    **** Challenge ****
+    
+    For this challenge destruture the following object.
+*/
+
+const student = {
+  name: "Kyle",
+  age: 24,
+  projects: {
+      diceGame: "Two player dice game using JavaScript"
+  }
+}
+
+const { 
+  name, age, projects: {diceGame} 
+} = student;
+
+console.log( name, age, diceGame);
+console.log(
+  `${name}, age ${age}, claims to have built what he calls a "${diceGame}"`
+  )
