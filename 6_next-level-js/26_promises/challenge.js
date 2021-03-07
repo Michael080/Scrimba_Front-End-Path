cc1f66a44d4a5408b1ace55aa0487a8bd1d359f2
Michael Rollin <mrollin808@gmail.com>
2021-03-06 22:20:32 -0700

    Promises tut/challenge.

--- 6_next-level-js/26_promises/challenge.js
+++ 6_next-level-js/26_promises/challenge.js
@@ -1,40 +1,58 @@
-// Promises
-// The Promise object represents the eventual completion (or failure) of 
-// an asynchronous operation and its resulting value
-const buyFlightTicket = () => {
-  // promise is stored in buyFlightTicket()
-  /* 
-  3 possible promise states:
-    * pending 
-    * fulfilled
-    * rejected
-  */
-// if everything goes well run resolve || else reject
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const error = false;
-
-      if (error) {
-        reject('Sorry your payment was not successful');
-      } else {
-        resolve('Thank you, your payment was successful');
-      }
-
-    }, 3000)
-  });
+const user = {
+  name: 'Bob Dobson',
+  age: 50
 }
 
-// When the process is fulfilled we want to know the outcome
-// then --- return w/o error/s
-// The then() method returns a Promise. 
-// It takes up to two arguments:    
-//  callback functions for the success and failure cases of the Promise.
+// alternate user:
+// const user = {
+//   name: 'Sally'
+// }
 
-// catch --- return w/ error/s
+console.log(user.hasOwnProperty('age'))
 
-// success & error in this case, could be called anything
-// success takes whatever is in resolve()
-// error takes whatever is in reject()
-buyFlightTicket()
-  .then((success) => console.log(success))
+function resolved(user, message) {
+  console.log(`${user.name} is ${user.age} years old.`);
+  console.log(message);
+}
+
+function rejected(message) {
+  console.log(message);
+}
+
+const checkAge = new Promise((resolve, reject) => {
+    if (user.hasOwnProperty('age')) {
+      resolved(user, 'Success!');
+    } else {
+      rejected('User age data missing :(');
+    }
+  })
+
+checkAge
+  .then(resolved)
+  .catch(() => rejected);
+
+
+  
+// -----------------------------------------------------------------
+//                     Scrimba Solution
+console.log('-----------------------------------');
+console.log('Scrimba Solution:');
+
+const userData = new Promise((resolve, reject) => {
+  // I didn't use a default bool
+  const error = false;
+
+  if(error) {
+    reject('500 Level Error');
+  } else {
+    resolve({
+      firstName: 'Dylan',
+      age: 44,
+      email: 'shmotmail@shmail.com'
+    });
+  }
+});
+console.log('blammo');
+userData
+  .then((data) => console.log(data))
   .catch((error) => console.log(error));
--- 6_next-level-js/26_promises/tut.js
+++ 6_next-level-js/26_promises/tut.js
@@ -1,40 +1,49 @@
-/**
+// Promises
+// The Promise object represents the eventual completion (or failure) of 
+// an asynchronous operation and its resulting value
+const buyFlightTicket = () => {
+  // promise is stored in buyFlightTicket()
+  /* 
+  3 possible promise states:
+    * pending 
+    * fulfilled
+    * rejected
+  */
+// if everything goes well run resolve || else reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const error = false;
+
+      if (error) {
+        reject('Sorry your payment was not successful');
+      } else {
+        resolve('Thank you, your payment was successful');
+      }
+
+    }, 3000)
+  });
+}
+
+// When the process is fulfilled we want to know the outcome
+// then --- return w/o error/s
+// The then() method returns a Promise. 
+// It takes up to two arguments:    
+//  callback functions for the success and failure cases of the Promise.
+
+// catch --- return w/ error/s
+
+// success & error in this case, could be called anything
+// success takes whatever is in resolve()
+// error takes whatever is in reject()
+buyFlightTicket()
+  .then((success) => console.log(success))
+  .catch((error) => console.log(error));
+
+
+  /**
  * Promises - Challenge
  * Create a promise that returns some user data and throws an error when not found.
  * Log the user data when listening to the promise as well as log the error.
  * 
  * Docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
 */
-
-const bob = {
-  name: 'Bob Dobson',
-  age: 50
-}
-
-const sally = {
-  name: 'Sally'
-}
-
-console.log(bob.hasOwnProperty('age'))
-
-function resolved(user, message) {
-  console.log(`${user.name} is ${user.age} years old.`);
-  console.log(message);
-}
-
-function rejected(message) {
-  console.log(message);
-}
-
-const checkAge = (user) => {
-  return new Promise((resolve, reject) => {
-    if (user.hasOwnProperty('age')) {
-      resolved(user, 'Success!');
-    } else {
-      rejected('User age data missing :(');
-    }
-  })
-}
-
-checkAge(bob); // => success
-checkAge(sally); // => failure
